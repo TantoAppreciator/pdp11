fd615c9c2ad86e34a9813ae7614c82a9fd3d12a1
Voronin <voronin.ov@phystech.edu>
2024-03-18 18:21:40 +0300

    added load_data, mem_dump, masked w_read and b_read

--- pdp11.c
+++ pdp11.c
@@ -11,62 +11,27 @@ void b_write(address adr, byte val);
 byte b_read(address adr);
 void w_write(address adr, word val);
 word w_read(address adr);
-void test_mem();
+void load_data(FILE *stream);
+void mem_dump(address adr, int size);
+
 int main()
 {
-    test_mem();
+    load_data(stdin);
+
+    mem_dump(0x40, 20);
+    printf("\n");
+    mem_dump(0x200, 0x26);
+
     return 0;
 }
-void test_mem()
-{
-    address a;
-    byte b0, b1, bres;
-    word w, wres;
 
-    // пишем байт, читаем байт
-    fprintf(stderr, "Пишем и читаем байт по четному адресу\n");
-    a = 0;
-    b0 = 0x12;
-    b_write(a, b0);
-    bres = b_read(a);
-    // тут полезно написать отладочную печать a, b0, bres
-    fprintf(stderr, "a=%06o b0=%hhx bres=%hhx\n", a, b0, bres);
-    assert(b0 == bres);
-    // аналогично стоит проверить чтение и запись по нечетному адресу
-
-    // пишем слово, читаем слово
-    fprintf(stderr, "Пишем и читаем слово\n");
-    a = 2; // другой адрес
-    w = 0x3456;
-    w_write(a, w);
-    wres = w_read(a);
-    // тут полезно написать отладочную печать a, w, wres
-    fprintf(stderr, "a=%06o w=%04x wres=%04x\n", a, w, wres);
-    assert(w == wres);
-
-    // пишем 2 байта, читаем 1 слово
-    fprintf(stderr, "Пишем 2 байта, читаем слово\n");
-    a = 4; // другой адрес
-    w = 0xa1b2;
-    // little-endian, младшие разряды по меньшему адресу
-    b0 = 0xb2;
-    b1 = 0xa1;
-    b_write(a, b0);
-    b_write(a + 1, b1);
-    wres = w_read(a);
-    // тут полезно написать отладочную печать a, w, wres
-    fprintf(stderr, "a=%06o b1=%02hhx b0=%02hhx wres=%04x\n", a, b1, b0, wres);
-    assert(w == wres);
-
-    // еще тесты
-}
 void b_write(address adr, byte val)
 {
     mem[adr] = val;
 }
 byte b_read(address adr)
 {
-    return mem[adr];
+    return mem[adr] & 0xFF;
 }
 void w_write(address adr, word val)
 {
@@ -82,5 +47,22 @@ word w_read(address adr)
     word w = b2;
     w = w << 8;
     w = w | (0xFF & b1);
-    return w;
+    return w & 0xFFFF;
+}
+void load_data(FILE *stream)
+{
+    address adr;
+    int n;
+    while (2 == fscanf(stream, "%x%x", &adr, &n))
+        for (int i = 0; i < n; i++)
+        {
+            unsigned int x;
+            scanf("%x", &x);
+            b_write(adr + i, x);
+        }
+}
+void mem_dump(address adr, int size)
+{
+    for (int i = 0; i < size; i += 2)
+        printf("%06o: %06o %04x\n", adr + i, w_read(adr + i) & 0xFFFF, w_read(adr + i) & 0xFFFF);
 }
