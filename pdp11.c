48e8623b0742d1a6b08abf458226f01f51f8587a
Voronin <voronin.ov@phystech.edu>
2024-03-18 17:21:36 +0300

    added w_write, fixed w_read

--- pdp11.c
+++ pdp11.c
@@ -70,6 +70,10 @@ byte b_read(address adr)
 }
 void w_write(address adr, word val)
 {
+    byte b1 = val;
+    mem[adr] = b1;
+    byte b2 = (val >> 8);
+    mem[adr + 1] = b2;
 }
 word w_read(address adr)
 {
@@ -77,6 +81,6 @@ word w_read(address adr)
     byte b2 = mem[adr + 1];
     word w = b2;
     w = w << 8;
-    w = (0xFF & b1);
+    w = w | (0xFF & b1);
     return w;
 }
