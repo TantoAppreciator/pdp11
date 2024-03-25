9bdc0cdc3a497f4c127886723bd7e91f05239a09
Voronin <voronin.ov@phystech.edu>
2024-03-25 18:06:25 +0300

    fixed log and load_file, added set_log_level

--- pdp11.c
+++ pdp11.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include <assert.h>
 #define MEMSIZE (64 * 1024)
+#define DEBUG 0
+#define TRACE 1
+#define INFO 2
+#define ERROR 3
+int log_level = 0;
 typedef char byte;
 typedef unsigned int Length;
 typedef short word;
 typedef word address;
 byte mem[MEMSIZE];
+word reg[8];
 void b_write(address adr, byte val);
 byte b_read(address adr);
 void w_write(address adr, word val);
@@ -15,19 +22,21 @@ void load_data(FILE *stream);
 void mem_dump(address adr, int size);
 void load_file(const char *filename);
 void usage(const char *progname);
+void log(int level, char *format, ...);
+int set_log_level(int level);
 
 int main(int argc, char *argv[])
 {
-    if (argc == 1)
+    /*if (argc == 1)
     {
         usage(argv[0]);
         exit(1);
     }
-    load_file(argv[1]);
+    load_file(argv[1]); // /home/olivee/C/test.txt
     mem_dump(0x40, 20);
     printf("\n");
-    mem_dump(0x200, 0x26);
-
+    mem_dump(0x200, 0x26);*/
+    log(INFO, "%d%d%d", 3, 4, 5);
     return 0;
 }
 
@@ -57,13 +66,14 @@ word w_read(address adr)
 }
 void load_data(FILE *stream)
 {
-    address adr;
-    int n;
-    while (2 == fscanf(stream, "%hx%x", &adr, &n))
+    unsigned int adr;
+    unsigned int n;
+    while (2 == fscanf(stream, "%x%x", &adr, &n))
         for (int i = 0; i < n; i++)
         {
             unsigned int x;
-            scanf("%x", &x);
+            fscanf(stream, "%x", &x);
+            printf("%x: %x\n", adr + i, x);
             b_write(adr + i, x);
         }
 }
@@ -86,4 +96,19 @@ void load_file(const char *filename)
 void usage(const char *progname)
 {
     printf("USAGE: %s flie\n file - PDP-11 execution file\n", progname);
+}
+void log(int level, char *format, ...)
+{
+    if (level < log_level)
+        return;
+    va_list ap;
+    va_start(ap, format);
+    vprintf(format, ap);
+    va_end(ap);
+}
+int set_log_level(int level)
+{
+    int prev_level = log_level;
+    log_level = level;
+    return prev_level;
 }
