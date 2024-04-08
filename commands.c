b84f5104cbc5ac067b6a3c5e2725658fb300895e
Voronin <voronin.ov@phystech.edu>
2024-04-08 19:58:20 +0300

    added get_mr

--- commands.c
+++ commands.c
@@ -5,6 +5,35 @@ Arg ss, dd;
 
 Arg get_mr(word w)
 {
+    Arg res;
+    int r = w & 7;
+    int m = (w >> 3) & 7;
+    switch (m)
+    {
+    case 0:
+        res.adr = r;
+        res.val = reg[r];
+        logger(TRACE, "R%d", r);
+        break;
+    case 1:
+        res.adr = reg[r];
+        res.val = w_read(res.adr);
+        logger(TRACE, "(R%d)", r);
+        break;
+    case 2:
+        res.adr = reg[r];
+        res.val = w_read(res.adr);
+        reg[r] += 2;
+        if (r == 7)
+            logger(TRACE, "#%o", res.val);
+        else
+            logger(TRACE, "(R%d)+", r);
+        break;
+    default:
+        logger(ERROR, "Mode %d is not implemented yet\n", m);
+        exit(1);
+    }
+    return res;
 }
 void do_nothing() {}
 void do_halt()
